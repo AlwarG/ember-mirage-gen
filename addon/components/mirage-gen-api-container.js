79a9b47a0503d2561c82ca0a00e5c9dc52c1ce00
alwar.g <alwar.g@zohocorp.com>
2020-12-04 11:36:23 +0530

    Alignment fixes

--- addon/components/mirage-gen-api-container.js
+++ addon/components/mirage-gen-api-container.js
@@ -6,6 +6,24 @@ import layout from '../templates/components/mirage-gen-api-container';
 export default Component.extend({
   layout,
   canShowDB: false,
+  isObjFormat: computed('selectedMirageResponse.data', function() {
+    let response = this.get('selectedMirageResponse.data');
+    return typeof response === 'object';
+  }),
+
+  stringifiedResponse: computed('selectedMirageResponse.data', 'isObjFormat', function() {
+    let response = this.get('selectedMirageResponse.data');
+    if (this.isObjFormat) {
+      let unwantedNodes = [...objFns.getAddedNodes];
+
+      unwantedNodes.push('srcRoot');
+      return JSON.stringify(response, (key, value) => {
+        return unwantedNodes.includes(key) ? undefined : value;
+      }, 2);
+    }
+    return response;
+  }),
+
   filteredResponses: computed('searchValue', 'mirageResponses.[]', function() {
     let { mirageResponses, searchValue } = this;
     mirageResponses = mirageResponses || [];
